1f73d12ef7cc36e194d303fb7ba7a4c445fbeb71
Rune580 <runemaster580@gmail.com>
2023-12-28 10:48:29 -0500

    fix for mods with a non-public Plugin class

--- LethalCompanyInputUtils/Utils/AssemblyUtils.cs
+++ LethalCompanyInputUtils/Utils/AssemblyUtils.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using BepInEx;
 
 namespace LethalCompanyInputUtils.Utils;
@@ -7,7 +10,7 @@ internal static class AssemblyUtils
 {
     public static BepInPlugin? GetBepInPlugin(this Assembly assembly)
     {
-        foreach (var type in assembly.GetExportedTypes())
+        foreach (var type in assembly.GetValidTypes())
         {
             var plugin = type.GetCustomAttribute<BepInPlugin>();
             if (plugin is not null)
@@ -15,5 +18,17 @@ internal static class AssemblyUtils
         }
 
         return null;
+    }
+
+    public static IEnumerable<Type> GetValidTypes(this Assembly assembly)
+    {
+        try
+        {
+            return assembly.GetTypes();
+        }
+        catch (ReflectionTypeLoadException e)
+        {
+            return e.Types.Where(type => type is not null);
+        }
     }
 }
